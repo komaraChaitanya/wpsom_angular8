84ff2985f46bfa6c5586171901f08456d11d2f71
vijaysharm0757 <vijaysharma0757@gmail.com>
2019-12-20 08:04:48 +0530

    wpsom login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AngularMaterialModule } from './core/material.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule ,
+    FlexLayoutModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/material.module.ts
@@ -0,0 +1,83 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+   MatButtonModule,
+   MatToolbarModule,
+   MatIconModule,
+   MatBadgeModule,
+   MatSidenavModule,
+   MatListModule,
+   MatGridListModule,
+   MatFormFieldModule,
+   MatInputModule,
+   MatSelectModule,
+   MatRadioModule,
+   MatDatepickerModule,
+   MatNativeDateModule,
+   MatChipsModule,
+   MatTooltipModule,
+   MatTableModule,
+   MatPaginatorModule,
+   MatSliderModule,
+   MatCardModule,
+   MatProgressSpinnerModule,
+   MatDialogModule,
+   MatMenuModule 
+} from '@angular/material';
+
+@NgModule({
+   imports: [
+      CommonModule,
+      MatButtonModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatSidenavModule,
+      MatBadgeModule,
+      MatListModule,
+      MatGridListModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatSelectModule,
+      MatRadioModule,
+      MatDatepickerModule,
+      MatNativeDateModule,
+      MatChipsModule,
+      MatTooltipModule,
+      MatTableModule,
+      MatPaginatorModule,
+      MatSliderModule,
+      MatCardModule,
+      MatProgressSpinnerModule,
+      MatDialogModule,
+      MatMenuModule 
+   ],
+   exports: [
+      MatButtonModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatSidenavModule,
+      MatBadgeModule,
+      MatListModule,
+      MatGridListModule,
+      MatInputModule,
+      MatFormFieldModule,
+      MatSelectModule,
+      MatRadioModule,
+      MatDatepickerModule,
+      MatChipsModule,
+      MatTooltipModule,
+      MatTableModule,
+      MatPaginatorModule,
+      MatSliderModule,
+      MatCardModule,
+      MatProgressSpinnerModule,
+      MatDialogModule,
+      MatMenuModule 
+   ],
+   providers: [
+      MatDatepickerModule,
+   ]
+})
+
+export class AngularMaterialModule { }
